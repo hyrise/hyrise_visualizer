5e2708f6b28389df96c0f689538f37886e44fee4
Max Bothe <mathebox@gmail.com>
2014-05-28 15:17:02 +0200

    Show list of stored procedures beside editor.

--- visualizer/public/js/ui/storedProcedureList.js
+++ visualizer/public/js/ui/storedProcedureList.js
@@ -4,76 +4,40 @@
 	}
 
 	hyryx.editor.StoredProcedureList.prototype = extend(hyryx.screen.AbstractUIPlugin, {
+		id: hyryx.utils.getID('StoredProcedureList'),
+		frame: $('<div class="col-md-' + 2 + '"></div>'),
 
-		render : function() {
-			return this.createStencilsMarkup();
-		},
-
-		init : function() {
-			this.load();
-		},
-
-		createStencilsMarkup : function() {
-			// create container for stencils
-			this.id = hyryx.utils.getID('Stencils');
-			var $stencils = $('<div class="col-md-2 stencils" id="'+this.id+'"><h3>Procedures</h3></div>').appendTo(this.targetEl);
-			$stencils.append('<div class="panel-group list">');
-
-			return $stencils;
-		},
-
-		load : function() {
-			var me = this;
-
-			// load available stencils
-			$.getJSON('operations.json', function(data) {
-				$.each(data, function(k, v) {
-					var i=0;
-					for (op in v) { ++i; };
-
-					if (i > 0) {
-						hyryx.stencils[k] = v;
-					};
+		render: function() {
+			var self = this;
+			$.get('js/templates/storedProcedureList.mst', function(template) {
+				var rendered = Mustache.render(template, {
+					id: self.id
 				});
-
-				me.updateOpList();
+				self.frame.append(rendered);
+				self.frame.on("click", "a.list-group-item", function () {
+					alert($(this).data('content'));
+				})
 			});
+			this.targetEl.append(this.frame);
+			return this.frame;
 		},
 
-		updateOpList : function() {
+		init: function() {
+			$.getJSON('procedures.json', this.updateProcedureList);
+		},
 
-			var me = this;
+		updateProcedureList: function(data) {
+			$('.storedProcedureList .list').html('');
 
-			$('.stencils .list').html('');
-
-			var header = 'Operations';
-			var panel = ['<div class="panel panel-default">',
-							'<div class="panel-heading">',
-								'<h4 class="panel-title"><a class="accordion-toggle" data-toggle="collapse" data-parent=".stencils .list" href="#collapse-',header,'">',
-								header,
-								'</h4>',
-							'</div>',
-							'<div id="collapse-',header,'" class="panel-collapse collapse list-group">'];
-
-			$.each(hyryx.stencils, function(key, value) {
-
-				var $buttonMarkup = me.getStencilButtonMarkup(value);
-
-				panel.push($buttonMarkup);
+			$.get('js/templates/storedProcedureList_content.mst', function(template) {
+				var rendered = Mustache.render(template, {
+					header: 'Operations',
+					parent: 'storedProcedureList',
+					procedures: data.procedures
+				});
+				$('.storedProcedureList .list').append(rendered);
+				$('.storedProcedureList .list .collapse.panel-collapse:first').addClass('in');
 			});
-
-			panel.push('</div></div>');
-			$(panel.join('')).appendTo('.stencils .list');
-
-			$('.stencils .list .collapse.panel-collapse:first').addClass('in');
-
-			hyryx.debug.dispatch('canvas.initDragDrop');
-		},
-
-		getStencilButtonMarkup : function(config) {
-			return ['<a class="list-group-item" data-key="'+config.type+'" data-type="'+config.type+'" draggable="true">',
-						config.type,
-					'</a>'].join('');
-		},
+		}
 	});
 })();
