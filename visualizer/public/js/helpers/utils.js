e2c6b400bd2cb916c3f50e6e7ff52dbf7a88b3f4
Max Bothe <mathebox@gmail.com>
2014-06-04 12:25:38 +0200

    Show explorer on page load.

--- visualizer/public/js/helpers/utils.js
+++ visualizer/public/js/helpers/utils.js
@@ -13,11 +13,11 @@ if ('undefined' === typeof hyryx) { var hyryx = {}; }
 
 		function showScreen(id) {
             var hash = (window.location.hash||'').trim().split('#')[1];
-            
-            id = (id||hash||'').trim() || 'explorer';
+
+            id = 'explorer';//(id||hash||'').trim() || 'explorer';
 			// var hash = window.location.hash;
 			// the default page
-			var $target = $('#tag a[href=#page-'+id+']');
+			var $target = $('.navbar-nav a[href=#page-'+id+']');
 
 			// if (hash.trim()) {
 			// 	var target = hash.replace('#', '#page-');
