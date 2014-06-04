ee658d662caba423d6f093c95a26d9bdf786f9fa
Franz Liedke <franz.liedke@student.hpi.uni-potsdam.de>
2014-06-04 17:39:20 +0200

    #2: Add simple query builder class.

--- js/helpers/database.js
+++ js/helpers/database.js
@@ -1,8 +1,16 @@
 (function() {
 
+	function randomKey() {
+		return Math.floor(Math.random() * 1000000);
+	}
+
 	hyryx.Database = {
 
 		runQuery : function(query) {
+			if (query instanceof this.Query) {
+				query = query.serialize();
+			}
+			
 			return $.ajax({
 				url : hyryx.settings.database + '/jsonQuery/',
 				type : 'POST',
@@ -16,10 +24,36 @@
 			});
 		},
 
-		Query : {
+		Query : function() {
+			return {
 
-			//
+				operators: {},
+				edges: [],
 
+				addOperator : function(key, op) {
+					// If only an op is specified, we generate a key and return it.
+					if (op === undefined) {
+						op = key;
+						key = randomKey();
+					}
+
+					this.operators[key] = op;
+
+					return key;
+				},
+
+				addEdge : function(from, to) {
+					this.edges.push([from, to]);
+				},
+
+				serialize : function() {
+					return {
+						operators: this.operators,
+						edges: this.edges
+					};
+				}
+
+			};
 		}
 
 	};
