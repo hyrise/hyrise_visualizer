496194631dc40eb99ddb3dbb73a8ef2c425cc20b
Franz Liedke <franz.liedke@student.hpi.uni-potsdam.de>
2014-06-11 14:43:06 +0200

    A bit more API documentation.

--- js/helpers/procedureApi.js
+++ js/helpers/procedureApi.js
@@ -5,22 +5,22 @@
 		"getAttributeVectors()": {
 			"!type": "fn()",
 			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
-			"!doc": ""
+			"!doc": "Access the underlying table storage object."
 		},
 		"valueId()": {
 			"!type": "fn(arg: string)",
 			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
-			"!doc": ""
+			"!doc": "Return the value ID struct with value ID and table ID."
 		},
 		"valueIdV()": {
 			"!type": "fn(arg: string)",
 			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
-			"!doc": ""
+			"!doc": "Return only the value ID of the value ID struct."
 		},
 		"resize()": {
 			"!type": "fn(size: int)",
 			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
-			"!doc": ""
+			"!doc": "Resize the table, if possible. Throw an exception otherwise."
 		},
 		"setValueInt()": {
 			"!type": "fn(column: string, value: int)",
@@ -52,10 +52,35 @@
 			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
 			"!doc": ""
 		},
+		"copyStructureModifiable()": {
+			"!type": "fn(column: string)",
+			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
+			"!doc": ""
+		},
+		"createPointerCalculator()": {
+			"!type": "fn(column: string)",
+			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
+			"!doc": ""
+		},
+		"buildVerticalTable()": {
+			"!type": "fn(table1: string, table2: string)",
+			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
+			"!doc": "Concatenate two tables and build a vertical table."
+		},
+		"include()": {
+			"!type": "fn(filename: string)",
+			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
+			"!doc": "Include a file and make all functions globally available."
+		},
+		"log()": {
+			"!type": "fn(message: string)",
+			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
+			"!doc": "Log a string to the HYRISE logging facilities."
+		},
 		"buildTable()": {
 			"!type": "fn(spec: Array, groups: Array)",
 			"!url": "http://hyrise.github.io/hyrise/queryexecution/v8ops.html#using-javascript-for-plan-operations",
-			"!doc": "Build a table, fool!"
+			"!doc": "Create a custom output table."
 		}
 	};
 
