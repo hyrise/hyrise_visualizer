e50dc61960a66d1e690de0e3f951efdb5e36960d
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2014-06-18 14:13:51 +0200

    refactor queryEditor, delete shared variables

--- js/ui/queryEditor.js
+++ js/ui/queryEditor.js
@@ -1,28 +1,30 @@
 (function() {
-	var stencils,
-		canvas;
 
 	// Extend the standard ui plugin
 	hyryx.editor.QueryEditor = function() {
 		hyryx.screen.AbstractUITemplatePlugin.apply(this, arguments);
+		this.stencils = undefined;
+		this.canvas = undefined;
 	};
 
 	function registerEvents() {
-		stencils.on("initDragDrop", function(stencils){
-			canvas.initDragDrop(stencils);
+		var self = this;
+		this.stencils.on("initDragDrop", function(stencils){
+			self.canvas.initDragDrop(stencils);
 		});
 	}
 
 	hyryx.editor.QueryEditor.prototype = extend(hyryx.screen.AbstractUITemplatePlugin, {
 		render : function(callback) {
+			var self = this;
 			$.get('templates/queryEditor.mst', function(template) {
 				var rendered = $(Mustache.render(template, {
 					width_stencils: 3,
 					width_canvas: 9
 				}));
 
-				stencils = new hyryx.debug.Stencils(rendered.find('#frame_stencils'));
-				canvas = new hyryx.debug.Canvas(rendered.find('#frame_canvas'), {
+				self.stencils = new hyryx.debug.Stencils(rendered.find('#frame_stencils'));
+				self.canvas = new hyryx.debug.Canvas(rendered.find('#frame_canvas'), {
 					showTitlebar: true,
 					showExecuteButton: false
 				});
@@ -39,11 +41,11 @@
 		registerEvents : function() {
 			var self = this;
 			$('button#hideQueryEditor').click(function() {
-				canvas.storeJsonInMarker();
+				self.canvas.storeJsonInMarker();
 				self.emit('hideQueryEditor');
 			});
 			$('button#revertQueryPlan').click(function() {
-				canvas.revertToInitialQueryPlan();
+				self.canvas.revertToInitialQueryPlan();
 			});
 		},
 
