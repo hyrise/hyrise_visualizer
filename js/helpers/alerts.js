b75b99cb96cf0de0a6a7cb5698a6d15a42e00126
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2014-06-20 16:32:35 +0200

    Alerts: enable progress bars

--- js/helpers/alerts.js
+++ js/helpers/alerts.js
@@ -12,26 +12,47 @@
 		$(this).remove();
 	});
 
+	function insertAlert(type, message, strong, progress) {
+		if(0 > $.inArray(type, AVAIL_TYPES)) {
+			console.error("This alert type does not exist!");
+			return;
+		}
+		var $new_alert = $(Mustache.render(template, {
+			type: type,
+			message: message,
+			strong: strong,
+			progress: progress
+		}));
+		$('.alerts').append($new_alert);
+		return $new_alert;
+	}
+
+	function deleteAlert($alert) {
+		if ($alert) {
+			$alert.fadeOut(function() {
+				$alert.remove();
+			});
+		}
+	}
+
 	hyryx.Alerts = {
 
 		add: function(type, message, strong) {
-			if(0 > $.inArray(type, AVAIL_TYPES)) {
-				console.error("This alert type does not exist!");
-				return;
-			}
-			var $new_alert = $(Mustache.render(template, {
-				type: type,
-				message: message,
-				strong: strong
-			}));
-			$('.alerts').append($new_alert);
-			return window.setTimeout(function() {
-				if ($new_alert) {
-					$new_alert.fadeOut(function() {
-						$new_alert.remove();
-					});
-				}
-			}, 3000);
+			var $new_alert = insertAlert(type, message, strong, false);
+			if (!$new_alert) {return;}
+
+			return window.setTimeout(
+				deleteAlert.bind(this, $new_alert),
+				3000
+			);
+		},
+
+		wait: function(type, message, strong, deferred) {
+			var $new_alert = insertAlert(type, message, strong, true);
+			if (!$new_alert) {return;}
+			deferred.always(
+				deleteAlert.bind(this, $new_alert)
+			);
 		}
 
 	};
@@ -39,14 +60,28 @@
 	$.each(AVAIL_TYPES, function(index, value) {
 		// defines "addSuccess", "addInfo", ...
 		// ([title,] message): title is bold
-		var functionName = "add" + value.charAt(0).toUpperCase() + value.slice(1);
-		hyryx.Alerts[functionName] = (function(){
+		var namePart = value.charAt(0).toUpperCase() + value.slice(1),
+			addFunctionName = "add" + namePart,
+			waitFunctionName = "wait" + namePart;
+
+		hyryx.Alerts[addFunctionName] = (function(){
 			return function(strong, message) {
 				if (arguments.length < 2) {
 					message = strong;
 					strong = undefined;
 				}
 				return hyryx.Alerts.add(value, message, strong);
+			};
+		})();
+
+		hyryx.Alerts[waitFunctionName] = (function(){
+			return function(strong, message, deferred) {
+				if (arguments.length < 3) {
+					deferred = message;
+					message = strong;
+					strong = undefined;
+				}
+				return hyryx.Alerts.wait(value, message, strong, deferred);
 			};
 		})();
 	});
