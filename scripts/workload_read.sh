f9165b5ff25ab64db94917ead9b2c21d3865a3f2
LRueckert <lars-rueckert@web.de>
2015-01-25 13:37:04 +0100

    fixed postdata directory

--- scripts/workload_read.sh
+++ scripts/workload_read.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #../ab/ab -G ab_reads.log -l 6 -v 1 -k -t 180 -n 9999999 -c 50 -r -m revenue_reads_8M.txt 192.168.30.112:6666/jsonQuery
-~/benchmark/ab/ab -v 0 -k -t 180 -n 9999999 -c $1 -r -p ./postdata.txt 127.0.0.1:6666/procedureRevenueSelect/ &
+~/benchmark/ab/ab -v 0 -k -t 180 -n 9999999 -c $1 -r -p ~hyrise_visualizer/scripts/postdata.txt 127.0.0.1:6666/procedureRevenueSelect/ &
